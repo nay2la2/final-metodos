57e7a444df11856ac19a92df295cf976bbbc68ab
nay2la2 <159393560+nay2la2@users.noreply.github.com>
2024-02-07 20:28:13 -0500

    luna

---
+++ final.py
@@ -0,0 +1,22 @@
+def es_par(numero):
+    if numero % 2 == 0:
+        return "El número es par"
+    else:
+        return "El número es impar"
+
+# Prueba y cobertura de sentencia
+print(es_par(4))
+
+# Prueba y cobertura de decisión
+print(es_par(3))
+
+# Prueba y cobertura de condición
+print(es_par(0))
+
+# Prueba y cobertura de camino
+print(es_par(4))
+print(es_par(3))
+print(es_par(0))
+
+
+
